c9e83b1e38c7f8d64cc2d75d6d74c17774be03e8
Berkay <ozdemirberkaytr@gmail.com>
2021-05-14 22:03:41 +0300

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,6 @@
 require('./src/assets/scss/init.scss');
 require('./static/css/prismjs/theme.min.css');
 
-import './src/sass/app.scss'
-import 'jquery/dist/jquery.min.js'
-import 'popper.js/dist/popper.min'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "@popperjs/core/dist/umd/popper.min.js";
